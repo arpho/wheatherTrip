7146d709bfd0be619b8e430af7bc12a2817ffc48
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-11 17:27:15 +0200

    weatherKey
    written file of keys: not tracked

--- src/services/weather-data.ts
+++ src/services/weather-data.ts
@@ -1,9 +1,10 @@
 import { WeatherResponse } from "../models/interfaces/weather";
 import { SettingsData } from "./settings-data";
 import { convertFromKelvin } from "../helpers/utils";
+import {weatherKey} from "../configs/keys"
 class WeatherDataController {
 public data: WeatherResponse;
-private apiKey: string = "YOUR_API_KEY_HERE";
+private apiKey: string = weatherKey;
 constructor() {}
 async load() {}
 async refreshWeather() {}
